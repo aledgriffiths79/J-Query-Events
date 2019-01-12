09ba4af0ea69e86ca53c6e413cec576274cbe53b
Aled Griffiths <aledgriffiths08@hotmail.co.uk>
2019-01-12 18:51:56 +0000

    first commit

---
+++ jquery-events-master/script.js
@@ -0,0 +1,34 @@
+	$(document).ready(function() {
+ 
+ 	/*$("button").on(function() {
+ 		$("#hidden").hide();
+ 		$("#hidden").show("slow");
+ 		$("#hidden").show("medium");
+ 		$("#hidden").show("fast");
+ 		$("#hidden").show(1000);
+	});
+	});*/
+	
+		$("#stream1_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});	
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+
+
+}); 
+
+
